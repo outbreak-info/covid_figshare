49a264ecbd8e7b9011da396bab6f0c0357acb938
Laura Hughes <flaneuse@users.noreply.github.com>
2020-05-21 11:25:39 -0700

    dump config copied from PDB parser

---
+++ dump.py
@@ -0,0 +1,24 @@
+import os
+
+import biothings, config
+biothings.config_for_app(config)
+from config import DATA_ARCHIVE_ROOT
+
+import biothings.hub.dataload.dumper
+
+
+class FigshareDumper(biothings.hub.dataload.dumper.LastModifiedHTTPDumper):
+    # type: resource
+    SRC_NAME = "covid_figshare"
+    SRC_URLS = []
+    # override in subclass accordingly
+    SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
+
+    __metadata__ = {
+        "src_meta": {
+            'license_url': 'https://figshare.com/terms',
+            'url': 'https://covid19.figshare.com/'
+        }
+    }
+
+    SCHEDULE = None  # crontab format schedule, if None, won't be scheduled
