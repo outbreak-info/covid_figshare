f59ca0e72ad8db63b16cb84f85ff845b765cfdb6
Laura Hughes <flaneuse@users.noreply.github.com>
2020-06-17 08:25:41 -0700

    add "date"

--- upload.py
+++ upload.py
@@ -7,7 +7,7 @@ import requests
 biothings.config_for_app(config)
 
 MAP_URL = "https://raw.githubusercontent.com/SuLab/outbreak.info-resources/master/outbreak_resources_es_mapping.json"
-MAP_VARS = ["@type", "author", "curatedBy", "dateCreated", "dateModified", "datePublished", "description", "distribution", "doi", "funding", "identifier", "isBasedOn", "keywords", "license", "name", "url"]
+MAP_VARS = ["@type", "author", "curatedBy", "date", "dateCreated", "dateModified", "datePublished", "description", "distribution", "doi", "funding", "identifier", "isBasedOn", "keywords", "license", "name", "url"]
 
 # when code is exported, import becomes relative
 try:
