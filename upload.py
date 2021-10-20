175b7e9fbdd18de0cff1d17be6873def03a07f75
ginger <gtsueng@gmail.com>
2021-10-20 12:11:20 -0700

    remove unneeded 'correction' field

--- upload.py
+++ upload.py
@@ -7,7 +7,7 @@ import requests
 biothings.config_for_app(config)
 
 MAP_URL = "https://raw.githubusercontent.com/SuLab/outbreak.info-resources/master/outbreak_resources_es_mapping_v3.json"
-MAP_VARS = ["@type", "author", "curatedBy", "date", "dateCreated", "dateModified", "datePublished", "description", "distribution", "doi", "funding", "identifier", "isBasedOn", "keywords", "license", "name", "url","correction","evaluations","topicCategory", "citedBy"]
+MAP_VARS = ["@type", "author", "curatedBy", "date", "dateCreated", "dateModified", "datePublished", "description", "distribution", "doi", "funding", "identifier", "isBasedOn", "keywords", "license", "name", "url","evaluations","topicCategory", "citedBy"]
 
 # when code is exported, import becomes relative
 try:
